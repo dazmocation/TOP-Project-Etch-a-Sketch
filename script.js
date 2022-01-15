643c82dc59b23c3512fcea334a42a99d72b85504
daz <dazmocation@gmail.com>
2022-01-15 18:33:16 -0500

    Got basic sketching working

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const sketchBoard = document.getElementById('sketchBoard');
+const square = document.getElementsByClassName('square');
 
 //create grid
 function createGrid() {
     for (i = 0; i < 256; i++){
         const square = document.createElement('div');
         square.className = "square";
+        square.addEventListener('mouseover',changeColor);
         sketchBoard.appendChild(square);
     }
 }
 
-console.log(sketchBoard);
+function changeColor(e) {
+    e.target.style.backgroundColor = "#333";
+}
+
 createGrid();

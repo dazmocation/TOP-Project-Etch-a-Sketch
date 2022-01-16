a2551d5cebf3a2a25ffef08d28040552379d9f20
daz <dazmocation@gmail.com>
2022-01-15 20:24:14 -0500

    Added many variables for getElement

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const sketchBoard = document.getElementById('sketchBoard');
 const square = document.getElementsByClassName('square');
+const gridOne = document.getElementById('button1');
+const gridTwo = document.getElementById('button2');
+const gridThree = document.getElementById('button3');
+const gridFour = document.getElementById('button4');
+const black = document.getElementById('black');
+const white = document.getElementById('white');
+const red = document.getElementById('red');
+const orange = document.getElementById('orange');
+const yellow = document.getElementById('yellow');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+const purple = document.getElementById('purple');
+
+
+//choose grid size
+
 
 //create grid
 function createGrid() {
@@ -12,7 +28,7 @@ function createGrid() {
 }
 
 function changeColor(e) {
-    e.target.style.backgroundColor = "#333";
+    e.target.style.backgroundColor = "black";
 }
 
 createGrid();
